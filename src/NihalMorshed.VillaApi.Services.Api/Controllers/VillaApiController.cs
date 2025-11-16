7858c0de0bf9b4c2e69a4a3c15f8b7b23443244f
Nihal Morshed <nilumorshed@gmail.com>
2025-11-17 01:10:47 +0600

    Added CreatedAtRoute to the post method

--- src/NihalMorshed.VillaApi.Services.Api/Controllers/VillaApiController.cs
+++ src/NihalMorshed.VillaApi.Services.Api/Controllers/VillaApiController.cs
@@ -15,7 +15,7 @@ namespace NihalMorshed.VillaApi.Services.Api.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("id")] // <- When more than one get method is available , we have to put "parameter" so that swagger knows they are different.
+        [HttpGet("id", Name = "GetVillaById")] // <- When more than one get method is available , we have to put "parameter" so that swagger knows they are different. "Name" needed for the CreatedAtRoute functionality 
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VillaDto))] // <- when using IActionResult; Type is defined here for documentation.
@@ -32,6 +32,29 @@ namespace NihalMorshed.VillaApi.Services.Api.Controllers
             }
 
             return Ok(vil);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest();
+            }
+            if (villa.Id != 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villa.Id = VillaStore.villaList.OrderByDescending(i => i.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villa);
+            return CreatedAtRoute("GetVillaById", new { villa.Id }, villa); // <- CreatedAtRoute returns a 201 Created response with a Location header of the URI of the newly created resource (used for POST methods)
 
         }
 
